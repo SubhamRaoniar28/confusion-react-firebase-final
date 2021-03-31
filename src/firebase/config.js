4ca9d7ca159ab07541e26cec85151c298f63221d
Subham Raoniar <subham.raoniar@gmail.com>
2021-04-01 00:03:35 +0530

    Final version - Integrated React client with firebase backend (Firestore, storage, authentication, hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+
+  apiKey: "Your API Key",
+  authDomain: "Your Auth Domain",
+  projectId: "Your Project ID",
+  storageBucket: "Your Storage Bucket",
+  messagingSenderId: "Your Messaging Sender ID",
+  appId: "YOUR APP ID"
+
+};
